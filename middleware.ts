9ec163b4165864715cc140b89e3d81199511aa04
divyKS <divycodescompetes@gmail.com>
2024-09-27 09:55:19 +0530

    hono middleware with clerkAuth, protecting routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server';
 
-const isProtectedRoute = createRouteMatcher(['/'])
+const isProtectedRoute = createRouteMatcher([
+	'/',
+	// '/api(.*)'
+])
 
 export default clerkMiddleware((auth, req) => {
     if(isProtectedRoute(req)){
