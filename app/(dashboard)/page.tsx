1db13e0fffe712a9556be128763a0ca17d13884c
divyKS <divycodescompetes@gmail.com>
2024-09-27 07:30:14 +0530

    signin, signup, logout done

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+    return (
+        <>
+            <UserButton/>
+            <p>
+                This is your dashboard, this is protected
+            </p>
+        </>
+    )
+}
