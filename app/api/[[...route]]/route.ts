e9bbeba493d56002792640bd666a2448f54f4188
divyKS <divycodescompetes@gmail.com>
2024-09-30 09:09:06 +0530

    setting up api like RPC for larger applications, one route ready

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,26 +1,34 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+
+import accounts from './accounts'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/',  (c) => {
-    return c.json({ message: "Root API Endpoint" })
-})
+// app.route('/accounts', accounts)
 
-app.get(
-    '/hello',
-    clerkMiddleware(),
-    (c) => {
-        const auth = getAuth(c)
-        if(!auth?.userId){
-            return c.json({ error: 'Unauthorized' })
-        }
-        return c.json({ message: 'Hello Next.js!', userId: auth.userId })
-    }
-)
+const routes = app
+    .route('/accounts', accounts)
+
+// app.get('/',  (c) => ( c.json({ message: "Root API Endpoint" }) ))
+// app.get(
+//     '/hello',
+//     clerkMiddleware(),
+//     (c) => {
+//         const auth = getAuth(c)
+//         if(!auth?.userId){
+//             return c.json({ error: 'Unauthorized' })
+//         }
+//         return c.json({ message: 'Hello Next.js!', userId: auth.userId })
+//     }
+// )
 
 export const GET = handle(app)
 export const POST = handle(app)
+
+export type AppType = typeof routes
+
+// export default app
+// export type AppType = typeof routes
