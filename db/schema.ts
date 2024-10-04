200c0a71ec0269f3ed5716763b4c2ea6742a4647
divyKS <divycodescompetes@gmail.com>
2024-10-04 10:36:00 +0530

    categories schema

--- db/schema.ts
+++ db/schema.ts
@@ -10,3 +10,8 @@ export const accounts = pgTable("accounts", {
 
 export const insertAccountSchema = createInsertSchema(accounts)
 
+export const categories = pgTable("categories", {
+    id: text("id").primaryKey(),
+    name: text("name").notNull(),
+    userId: text("user_id").notNull(),
+})
