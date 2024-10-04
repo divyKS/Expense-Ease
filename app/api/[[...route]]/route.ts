16aa2dffda77040cdf73fcd34312b0e11a33361a
divyKS <divycodescompetes@gmail.com>
2024-10-04 15:58:52 +0530

    route for /transactions

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,6 +3,7 @@ import { handle } from 'hono/vercel'
 
 import accounts from './accounts'
 import categories from './categories'
+import transactions from './transactions'
 import { HTTPException } from 'hono/http-exception'
 
 export const runtime = 'edge'
@@ -14,6 +15,7 @@ const app = new Hono().basePath('/api')
 const routes = app
     .route('/accounts', accounts)
     .route('/categories', categories)
+    .route('/transactions', transactions)
 
 // app.get('/',  (c) => ( c.json({ message: "Root API Endpoint" }) ))
 // app.get(
