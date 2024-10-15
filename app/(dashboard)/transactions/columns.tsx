d103eb884ecfd901e3fccc5fa9186e6a2eb4e7f1
divyKS <divycodescompetes@gmail.com>
2024-10-15 09:23:05 +0530

    /dashboard route, copied from accounts

---
+++ app/(dashboard)/transactions/columns.tsx
@@ -0,0 +1,56 @@
+// not exactly re-useable, is diff for each data column
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
+import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown } from "lucide-react"
+
+import { InferResponseType } from 'hono'
+import { client } from "@/lib/hono"
+import { Action } from "./actions"
+
+export type ResponseType = InferResponseType<typeof client.api.accounts.$get, 200>['data'][0] // hence we should write the status codes in hono error
+
+export const columns: ColumnDef<ResponseType>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Name
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => <Action id={row.original.id}/>
+  }
+]
---
+++ app/(dashboard)/transactions/page.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts"
+import { Loader2, Plus } from "lucide-react"
+import { DataTable } from "@/components/DataTable"
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts"
+import { Skeleton } from "@/components/ui/skeleton"
+import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete-accounts"
+import { columns } from "./columns"
+import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction"
+
+
+const TransactionsPage = () => {
+    const newAccount = useNewTransaction()
+    const accountQuery = useGetAccounts()
+    const deleteAccounts = useBulkDeleteAccounts()
+    const accounts = accountQuery.data || []
+
+    const isDisabled = accountQuery.isLoading || deleteAccounts.isPending
+
+    if(accountQuery.isLoading){
+        return (
+            <div className="mx-auto max-w-screen-2xl w-full  -mt-6 pb-10">
+                <Card className="border-none drop-shadow-sm">
+                    <CardHeader>
+                        <Skeleton className="h-8 w-48"/>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="h-[500px] w-full flex items-center justify-center">
+                            <Loader2 className="size-6 text-slate-300 animate-spin"/>
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+        )
+    }
+
+    return (
+        <div className="mx-auto max-w-screen-2xl w-full  -mt-6 pb-10"> {/* mx-auto max-w-screen-2xl so that on too large screen we stay in middle*/}
+            <Card className="border-none drop-shadow-sm">
+                <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+                    <CardTitle className="line-clamp-1 text-xl">Transaction History</CardTitle>
+                    <Button size="sm" onClick={newAccount.onOpen}>
+                        <Plus className="mr-2 size-4" /> Add new
+                    </Button>
+                </CardHeader>
+                <CardContent>
+                    <DataTable
+                        columns={columns}
+                        data={accounts}
+                        filterKey="name"
+                        disabled={isDisabled}
+                        onDelete={(row) => {
+                            const ids = row.map((r) => r.original.id)
+                            deleteAccounts.mutate({ids})
+                        }}
+                    />
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
+
+export default TransactionsPage
