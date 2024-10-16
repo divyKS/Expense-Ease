548f55f8584acf25bbb8c02e8a33d405baeecc19
divyKS <divycodescompetes@gmail.com>
2024-10-16 09:55:15 +0530

    remaining field for the transaction table

--- app/(dashboard)/transactions/columns.tsx
+++ app/(dashboard)/transactions/columns.tsx
@@ -10,6 +10,8 @@ import { InferResponseType } from 'hono'
 import { client } from "@/lib/hono"
 import { Action } from "./actions"
 import { format } from "date-fns"
+import { formatCurrency } from "@/lib/utils"
+import { Badge } from "@/components/ui/badge"
 
 export type ResponseType = InferResponseType<typeof client.api.transactions.$get, 200>['data'][0]
 
@@ -52,6 +54,101 @@ export const columns: ColumnDef<ResponseType>[] = [
     cell: ({ row }) => {
       const date = row.getValue("date") as Date;
       return <span>{format(date, "dd MMMM, yyyy")}</span>;
+    },
+  },
+  {
+    accessorKey: "category",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Category
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return (
+        <span>
+          {row.original.category}
+        </span>
+      )
+      // return (
+      //   <CategoryColumn
+      //     id={row.original.id}
+      //     category={row.original.category}
+      //     categoryId={row.original.categoryId}
+      //   />
+      // );
+    },
+  },
+  {
+    accessorKey: "payee",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Payee
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "amount",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Amount
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+
+      return (
+        <Badge
+          variant={amount < 0 ? "destructive" : "primary"}
+          className="px-2.5 py-2.5 text-xs font-medium"
+        >
+          {formatCurrency(amount)}
+        </Badge>
+      );
+    },
+  },
+  {
+    accessorKey: "account",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Account
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return (
+        <span>
+          {row.original.account}
+        </span>
+      )
+      // return (
+      //   <AccountColumn
+      //     account={row.original.account}
+      //     accountId={row.original.accountId}
+      //   />
+      // );
     },
   },
   {
