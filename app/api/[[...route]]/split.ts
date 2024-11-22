dc2023ee665fccc675f046d8864d25465115fdb2
divyKS <divycodescompetes@gmail.com>
2024-11-22 12:30:57 +0530

    create group, end points for the home page of split done

--- app/api/[[...route]]/split.ts
+++ app/api/[[...route]]/split.ts
@@ -4,18 +4,18 @@ import { db } from "@/db/drizzle"
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth"
 import { zValidator } from "@hono/zod-validator"
 import { z } from "zod"
-import { users } from "@/db/schema"
+import { groupMembers, groups, users } from "@/db/schema"
 
 const app = new Hono()
     .post('/create-user',
         clerkMiddleware(),
         zValidator("json", z.object({name: z.string().min(1, "Name is required")})),
         async (c) => {
-            const auth = getAuth(c)
+            // const auth = getAuth(c)
 
-            if(!auth?.userId){
-                return c.json({ error: "Unauthorized" }, 401)
-            }
+            // if(!auth?.userId){
+            //     return c.json({ error: "Unauthorized" }, 401)
+            // }
 
             const { name } = c.req.valid("json")
 
@@ -26,11 +26,11 @@ const app = new Hono()
     .get('/users',
         clerkMiddleware(),
         async (c) => {
-            const auth = getAuth(c);
+            // const auth = getAuth(c);
         
-            if (!auth?.userId) {
-              return c.json({ error: "Unauthorized" }, 401);
-            }
+            // if (!auth?.userId) {
+            //   return c.json({ error: "Unauthorized" }, 401);
+            // }
         
             const userList = await db.select().from(users)
 
@@ -39,6 +39,46 @@ const app = new Hono()
             return c.json(userList, 200)
         }
     )
-    
+    .post(
+        "/create-group",
+        clerkMiddleware(),
+        zValidator(
+          "json",
+          z.object({
+            name: z.string().min(1, "Group name is required"),
+            userIds: z.array(z.number().int()).nonempty("At least one user must be selected"),
+          })
+        ),
+        async (c) => {
+        //   const auth = getAuth(c);
+      
+        //   if (!auth?.userId) {
+        //     return c.json({ error: "Unauthorized" }, 401);
+        //   }
+      
+          const { name, userIds } = c.req.valid("json");
+
+          const [createdGroup] = await db.insert(groups).values({ name }).returning()
+          const membersData = userIds.map((userId) => ({
+            group_id: createdGroup.group_id,
+            user_id: userId,
+          }))
+          const addedMembers = await db.insert(groupMembers).values(membersData).returning();
+
+          return c.json({
+              group: createdGroup,
+              members: addedMembers,
+            }, 201)
+
+
+    //    await db.transaction(async (trx) => {
+    //         const [createdGroup] = await trx.insert(groups).values({ name }).returning()
+    //         await trx.insert(groupMembers).values(membersData)
+    //     })
+
+        
+        }
+      )
+
 
 export default app
