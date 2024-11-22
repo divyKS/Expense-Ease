f402e000c087f787aaf4d9d8afe191bac57bde05
divyKS <divycodescompetes@gmail.com>
2024-11-22 12:02:58 +0530

    create user and get all user endpoint

--- app/api/[[...route]]/split.ts
+++ app/api/[[...route]]/split.ts
@@ -4,18 +4,41 @@ import { db } from "@/db/drizzle"
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth"
 import { zValidator } from "@hono/zod-validator"
 import { z } from "zod"
+import { users } from "@/db/schema"
 
 const app = new Hono()
-    .get('/',
+    .post('/create-user',
         clerkMiddleware(),
+        zValidator("json", z.object({name: z.string().min(1, "Name is required")})),
         async (c) => {
             const auth = getAuth(c)
 
             if(!auth?.userId){
                 return c.json({ error: "Unauthorized" }, 401)
             }
+
+            const { name } = c.req.valid("json")
+
+            const newUser = await db.insert(users).values({name}).returning()
             
-            return c.json({ data: "route setup done" }, 200)
+            return c.json(newUser[0], 201)
         })
+    .get('/users',
+        clerkMiddleware(),
+        async (c) => {
+            const auth = getAuth(c);
+        
+            if (!auth?.userId) {
+              return c.json({ error: "Unauthorized" }, 401);
+            }
+        
+            const userList = await db.select().from(users)
+
+            if(!userList)  return c.json({ error: "Users not found." }, 404)
+        
+            return c.json(userList, 200)
+        }
+    )
+    
 
 export default app
